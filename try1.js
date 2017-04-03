6c7de96cc7bba7f576741b601ad08b677e979af9
metemorris <metemorris@hotmail.com>
2017-04-03 16:15:17 -0500

    wrote insert into

--- try1.js
+++ try1.js
@@ -1,9 +1,9 @@
 var mysql      = require('mysql');
 var express    = require("express");
 var connection = mysql.createConnection({
-  host     : '',//enter database IP
-  user     : '',//enter your username
-  password : '',//enter your password
+  host     : '188.121.44.185',//enter database IP
+  user     : 'metemorris',//enter your username
+  password : 'CS425Team13!',//enter your password
   database : 'metemorris_CS425'
 });
 var app = express();
@@ -16,9 +16,18 @@ if(!err) {
 }
 });
 
+//sample SELECT lookup
 connection.query('SELECT * from Admin', function(err, rows, fields) {
   if (!err)
     console.log('The solution is: ', rows);
+  else
+    console.log('Error while performing Query.');
+});
+
+//sample insert into
+connection.query('INSERT INTO `Admin`(`Admin_ID`, `Password`) VALUES ("Morgan","ASDFG")', function(err) {
+  if (!err)
+    console.log('The solution is: ');
   else
     console.log('Error while performing Query.');
 });
