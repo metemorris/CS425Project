c97874154508b85e9471fc93043c33efe31c912d
metemorris <metemorris@hotmail.com>
2017-04-04 13:06:03 -0500

    made functions nice and also tried doing post but didnt work

--- routes/admin.js
+++ routes/admin.js
@@ -1,6 +1,8 @@
 var mysql      = require('mysql');
 const express = require("express");
 const router = express.Router();
+const sqlFunctions = require("./functions/sqlFunctions.js");
+
 
 
 //update admin name
@@ -11,41 +13,20 @@ const router = express.Router();
 
 //display the admins
 router.get('',function (req,res){
-  var connection = mysql.createConnection({
-    host     : '188.121.44.185',//enter database IP
-    user     : 'metemorris',//enter your username
-    password : 'CS425Team13!',//enter your password
-    database : 'metemorris_CS425'
+  sqlFunctions.selectFunction("SELECT * from Admin",function(response){
+    res.json(response);
   });
-  connection.connect(
-    function(err){
-      if(!err) {
-        console.log("Database is connected ... nn");
-        connection.query('SELECT * from Admin', function(err, rows, fields) {
-          if (!err){
-            res.json(rows);
-            connection.end();
-          }
-          else{
-            console.log('Error while performing Query.');
-            connection.end();
-          }
-        });
-      }
-    else {
-      console.log("Error connecting database ... nn"+err);
-    }
-  });
-
-
-
-
 })
 
 
 //delete admin
 
 //add admin
+router.post('',function (req,res){
+  select.INSERT('INSERT INTO `Admin`(`Admin_ID`, `Password`) VALUES ("anothr","MALYALAM")',function(response){
+    res.json(response);
+  });
+})
 
 
 module.exports = router;
---
+++ routes/functions/sqlFunctions.js
@@ -0,0 +1,69 @@
+var mysql      = require('mysql');
+const express = require("express");
+
+//SELECT FUNCTION
+var selectFunction = function (selectStatement,callback){
+  var connection = mysql.createConnection({
+  host     : '188.121.44.185',//enter database IP
+  user     : 'metemorris',//enter your username
+  password : 'CS425Team13!',//enter your password
+  database : 'metemorris_CS425'
+  });
+  connection.connect(
+    function(err){
+      if(!err) {
+        console.log("Database is connected ...");
+        connection.query(selectStatement, function(err, rows, fields) {
+          if (!err){
+            connection.end();
+            return callback(rows);
+          }
+          else{
+            console.log('Error while performing Query.'+err);
+            connection.end();
+            //return 'Error while performing Query.'+err;
+          }
+        });
+      }
+      else {
+        console.log("Error connecting database ... nn"+err);
+        //return "Error connecting database ... nn"+err;
+      }
+    });
+}
+
+//INSERT FUNCTION
+var insertFunction = function (insertStatement,callback){
+  var connection = mysql.createConnection({
+  host     : '188.121.44.185',//enter database IP
+  user     : 'metemorris',//enter your username
+  password : 'CS425Team13!',//enter your password
+  database : 'metemorris_CS425'
+  });
+  connection.connect(
+    function(err){
+      if(!err) {
+        console.log("Insert Database is connected ...");
+        connection.query(insertStatement, function(err) {
+          if (!err){
+            connection.end();
+            return callback("Value "+insertStatement+"inserted");
+          }
+          else{
+            //console.log('Error while performing Query.'+err);
+            connection.end();
+            return callback('Error while performing Query.'+err);
+            //return 'Error while performing Query.'+err;
+          }
+        });
+      }
+      else {
+        return callback("Error connecting database ... nn"+err);
+        //return "Error connecting database ... nn"+err;
+      }
+    });
+}
+
+
+module.exports.selectFunction = selectFunction;
+module.exports.insertFunction = insertFunction;
