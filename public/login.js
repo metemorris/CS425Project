7267db8f04a29bc2def5ac6c522b83fb80dcfae3
Morgan <mwilso20@hawk.iit.edu>
2017-04-11 19:58:35 -0500

    gonna populate the rest, just taking the first step

--- public/login.js
+++ public/login.js
@@ -1,27 +1,31 @@
-function validate() {
-  var username = document.getElementById("username").value;
-  var password = document.getElementById("password").value;
-
-  if ( username == "test" && password == "test") {
-  alert ("Login successfully");
-  
-  window.location = "admin.html"; // Redirecting to other page.
-  return false;
-  }
-}
+// function validate() {
+//   var username = document.getElementById("username").value;
+//   var password = document.getElementById("password").value;
+//
+//   if ( username == "test" && password == "test") {
+//   alert ("Login successfully");
+//
+//   window.location = "admin.html"; // Redirecting to other page.
+//   return false;
+//   }
+// }
 
 function toggleStudent() {
-
+  alert ("You're a student.")
+  window.location = "student.html";
 }
 
 function toggleParent() {
-
+  alert ("You're a parent.")
+  window.location = "parent.html";
 }
 
 function toggleTeacher() {
-
+  alert ("You're a teacher.")
+  window.location = "teacher.html";
 }
 
 function toggleAdmin() {
-
+  alert ("You're an admin.")
+  window.location = "admin.html";
 }
