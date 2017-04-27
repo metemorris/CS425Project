da786f6e0d47b0148f03d92f372c0fa1794142f1
metemorris <metemorris@hotmail.com>
2017-04-26 23:35:44 -0500

    parent crud done

--- routes/parent.js
+++ routes/parent.js
@@ -24,23 +24,44 @@ router.get('/books/:id',function (req,res){
 
 
 
-//CREATE PARENT
+//CREATE PARENT works
 router.post('',function (req,res){
   console.log(req.body);
-  var id= req.body.parent_id;
   var password = req.body.password;
   var firstname= req.body.fname;
   var lastname = req.body.lname;
   var middlename= req.body.mname;
   var studentid =req.body.sid;
-  console.log(id+password)
-  sqlFunctions.selectFunction("INSERT INTO ParentGuardian VALUES ("+id+",'"+firstname+",'"+lastname+",'"+middlename+",'"+password+",'"+studentid+")",function(response){
+  sqlFunctions.selectFunction("INSERT INTO `ParentGuardian`(`First_Name`, `Last_Name`, `Middle_Name`, `Password`, `Student_ID`) VALUES ('"+firstname+"','"+lastname+"','"+middlename+"','"+password+"',"+studentid+")",function(response){
 
     res.json(response); 
   });
 })
 
 
+//deleteparent
+router.delete('/:id',function (req ,res){
+  var userid = req.params.id;
+  console.log(userid)
+  sqlFunctions.selectFunction("DELETE FROM ParentGuardian WHERE ParentGuardian_ID="+userid,function(response){
+    res.json(response);
+  });
+})
+
 //UPDATE PARENT
+router.post('/update',function (req,res){
+  console.log(req.body);
+  var userid = req.body.id;
+  var password = req.body.password;
+  var firstname= req.body.fname;
+  var lastname = req.body.lname;
+  var middlename= req.body.mname;
+  sqlFunctions.selectFunction("UPDATE `ParentGuardian` SET `First_Name`='"+firstname+"',`Last_Name`='"+lastname+"',`Middle_Name`='"+middlename+"',`Password`='"+password+"' WHERE ParentGuardian_ID="+userid,function(response){
+    res.json(response); 
+  });
+})
+
+
+
 
 module.exports = router;
